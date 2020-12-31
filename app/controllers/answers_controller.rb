28fbfb7d17e0f1eafdac9a2da2dda38470e70dee
yuki-nakagawa77 <tuna27kan789@gmail.com>
2020-12-31 12:59:01 +0900

    answersコントローラー記述

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,34 +1,37 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  layout 'Q&A'
 
-  # GET /answers
-  # GET /answers.json
   def index
-    @answers = Answer.all
+    redirect_to '/questions'
   end
 
-  # GET /answers/1
-  # GET /answers/1.json
   def show
+    redirect_to '/questions'
   end
 
-  # GET /answers/new
   def new
     @answer = Answer.new
   end
 
-  # GET /answers/1/edit
   def edit
+    redirect_to '/questions'
   end
 
-  # POST /answers
-  # POST /answers.json
   def create
+    end_counter = 10 # 終了にする回答数
     @answer = Answer.new(answer_params)
-
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        num = Answer.where('question_id = ?',@answer.question_id).count
+        if num >= end_counter then
+          q = Question.find @answer.question_id
+          q.finished = true
+          q.save
+        end
+        format.html {
+          redirect_to '/questions/' + @answer.question_id.to_s
+        }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new }
@@ -37,38 +40,21 @@ class AnswersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
   def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to '/questions'
   end
 
-  # DELETE /answers/1
-  # DELETE /answers/1.json
   def destroy
-    @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to '/questions'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def answer_params
-      params.require(:answer).permit(:question_id, :content, :name)
-    end
+  def answer_params
+    params.require(:answer).permit(:question_id, :content, :name)
+  end
 end
+
